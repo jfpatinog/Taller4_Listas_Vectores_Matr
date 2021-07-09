a2bf85882abacdd6000b488bb27eea4aeff0478f
jfpatinog <73499573+jfpatinog@users.noreply.github.com>
2021-07-09 15:18:52 -0500

    Add files via upload

---
+++ tallerListas-main/src/main/java/com/tcs/tallerListas/beans/Estudiante.java
@@ -0,0 +1,46 @@
+package com.tcs.tallerListas.beans;
+
+public class Estudiante {
+
+    String identificacion;
+    String nombre;
+    Integer edad;
+    String gradoACursar;
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public String getGradoACursar() {
+        return gradoACursar;
+    }
+
+    public void setGradoACursar(String gradoACursar) {
+        this.gradoACursar = gradoACursar;
+    }
+        
+    
+        
+	
+
+}
